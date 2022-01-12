9f26ff1bb3bdc093ecfa70bd025e608ca7740579
TanninOne <seppleviathan@gmx.de>
2022-01-12 14:21:06 +0100

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
 let webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('gamebryo-plugin-indexlock', __dirname, 4);
+module.exports = webpack('gamebryo-plugin-indexlock', __dirname, 5);
